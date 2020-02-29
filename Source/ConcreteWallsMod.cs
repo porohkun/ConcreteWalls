000e4f97d52151d66e51a0cc850f17d696942591
poroh-kun <meta.poroh.kun@gmail.com>
2020-03-01 01:43:25 +0300

    added option for allow/disallow build embrasures in mod settings

---
+++ Source/ConcreteWallsMod.cs
@@ -0,0 +1,36 @@
+﻿using HugsLib;
+using HugsLib.Settings;
+using RimWorld;
+using Verse;
+
+namespace ConcreteWalls
+{
+    public class ConcreteWallsMod : ModBase
+    {
+        public override string ModIdentifier => "ConcreteWalls";
+
+        private SettingHandle<bool> _allowEmbrasures;
+
+        public override void DefsLoaded()
+        {
+            _allowEmbrasures = Settings.GetHandle("allowEmbrasures",
+                "ConcreteWalls_AllowEmbrasuresName".Translate(),
+                "ConcreteWalls_AllowEmbrasuresDesc".Translate(),
+                true);
+            ChangeDefs();
+        }
+
+        public override void SettingsChanged()
+        {
+            base.SettingsChanged();
+            ChangeDefs();
+        }
+
+        private void ChangeDefs()
+        {
+            var embrasureThingDef = ThingDef.Named("Embrasure");
+            embrasureThingDef.designationCategory = _allowEmbrasures ? DesignationCategoryDefOf.Structure : null;
+            DesignationCategoryDefOf.Structure.ResolveReferences();
+        }
+    }
+}
